c6497799ad279366d9d909e8ce85c45265971eb0
ahmed007dr <ahmed007eg@gmail.com>
2023-11-19 18:12:17 +0200

    print error

--- plan11.py
+++ plan11.py
@@ -1,3 +1,11 @@
-age=int(input('enter ur age '))
 
-print (100/age)
+try:
+    age=int(input('enter ur age '))
+    print (age/100)
+
+except ValueError as e:
+    print('please enter number')
+    print(e)
+
+
+

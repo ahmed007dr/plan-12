b81f35d0367ae34bdf138fda19bd1b5fec549bd1
ahmed007dr <ahmed007eg@gmail.com>
2023-11-19 18:48:49 +0200

    add file to read

--- plan11.py
+++ plan11.py
@@ -1,14 +1,16 @@
 
-try:
-    age=int(input('enter ur age '))
-    print (100/age)
+# try:
+#     age=int(input('enter ur age '))
+#     print (100/age)
 
-# except (ZeroDivisionError,ValueError) as e:
+# # except (ZeroDivisionError,ValueError) as e:
+# #     print('please enter number')
+
+# except Exception:
 #     print('please enter number')
 
-except Exception:
-    print('please enter number')
 
-
+file=open('F:\django-5\plan 11\plan11.txt')
+print(file.readline())
 
 

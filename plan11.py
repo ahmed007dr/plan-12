2c8e8114d916c5221fcfd5a7bff0937b86a2b8c8
ahmed007dr <ahmed007eg@gmail.com>
2023-11-19 18:29:02 +0200

    print error3  exception

--- plan11.py
+++ plan11.py
@@ -3,9 +3,11 @@ try:
     age=int(input('enter ur age '))
     print (100/age)
 
-except (ZeroDivisionError,ValueError) as e:
-    print('please enter number')
+# except (ZeroDivisionError,ValueError) as e:
+#     print('please enter number')
 
+except Exception:
+    print('please enter number')
 
 
 
